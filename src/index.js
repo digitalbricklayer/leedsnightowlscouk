3f21d14e61897081653a8e75f8748c5814cd7681
Jack Hughes <jack.hughes@techteapot.com>
2025-12-29 11:36:21 +0000

    Strip www sub domain from redirect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 export default {
   async fetch(request) {
-    const externalHostname = "leedsnightowls.com";
+    const newSite = "leedsnightowls.com";
+    const oldSite = "leedsnightowls.co.uk";
+
+    const requestURL = new URL(request.url);
+    const path = requestURL.pathname;
+
+    // Strip the leading www. from the beginning of the URL
+    if (path.startsWith("https://www.")) {
+      // Redirect to new site without the leading www. sub-domain
+      const requestUrlWoutSubdomain = request.url.replace("https://www.", "https://");
+      return Response.redirect(requestUrlWoutSubdomain, 301);
+    }
     
     // Redirect request as is except to new website
-    const default_redirect = request.url.replace("leedsnightowls.co.uk", externalHostname);
+    const default_redirect = request.url.replace(oldSite, newSite);
     return Response.redirect(default_redirect, 301);
   },
 };
